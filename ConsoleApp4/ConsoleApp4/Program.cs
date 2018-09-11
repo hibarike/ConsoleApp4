4fe4f8a6f8b628a995a196d1f9695b62d52a7bdd
Boris Belyy <hibarike@gmail.com>
2018-09-11 12:29:14 +0300

    Добавил немного кода

--- ConsoleApp4/ConsoleApp4/Program.cs
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 
+
 namespace ConsoleApp4
 {
     class Program
@@ -7,6 +8,10 @@ namespace ConsoleApp4
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Class1 c = new Class1();
+            int a = c.post;
+            Console.WriteLine("{0},{1}",c.post,a);
+            
         }
     }
 }
