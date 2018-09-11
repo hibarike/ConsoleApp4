bab4d13e0a318703de0b8649ebbbf8a9fea88ad8
Boris Belyy <hibarike@gmail.com>
2018-09-11 12:29:02 +0300

    Создал новый класс

---
+++ ConsoleApp4/ConsoleApp4/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp4
+{
+    class Class1
+    {
+      public  int post;
+     public   Class1() { post = 0; }
+        
+    }
+}
